aa818ac65f6e73897f9e25ca532b359dbd706804
Ayanda Mbuthu <ayandambuthu@yahoo.com>
2023-02-28 19:42:31 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,17 @@
 
 void print_rev(char *s)
 {
-	int i, len = 0;
+	int len = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + len) != '\0')
+	{
 		len++;
-
-		for (i = len - 1; i >= 0; i--)
-			_putchar(s[i]);
-
+	}
+	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
 	_putchar('\n');
 }
