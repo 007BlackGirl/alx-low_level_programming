526ad036905a22e66d5db2018e7bc7b0605af4de
Ayanda Mbuthu <ayandambuthu@yahoo.com>
2023-03-06 17:11:50 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchar - locates character string
+ * _strchr - locates character string.
  * @s: string
  * @c: first occurrence of character
  * Return: pointer to first occurrence of c if there is or NULL if otherwise
@@ -15,6 +15,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		}
 	} while (*s++);
-
 	return (NULL);
 }
